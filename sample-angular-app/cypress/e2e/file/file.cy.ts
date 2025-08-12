d6be70068150416a4a68e5c503cac43b47f7455a
Gaurav Sharma <gaurav.sharma@riverty.com>
2025-08-12 16:23:32 +0200

    Update file.cy.ts

--- sample-angular-app/cypress/e2e/file/file.cy.ts
+++ sample-angular-app/cypress/e2e/file/file.cy.ts
@@ -3,7 +3,6 @@
 describe('File Upload/Download', () => {
   it('uploads a file (incorrect way)', () => {
     cy.visit('/file')
-    // INCORRECT: .type() does not work for file inputs in Cypress
     cy.get('input[type="file"]').type('cypress/fixtures/sample.txt')
     cy.get('button#upload-btn').click()
     cy.contains('File Uploaded!').should('exist')
